00d4943aeca7fab0614b132670decebae9d96df9
Jakub Adamek <jakubadamek@gmail.com>
2013-01-12 10:34:39 -0500

    oprava lastminute.es

--- src/main/java/com/jakubadamek/robotemil/htmlparser/LastminuteEs.java
+++ src/main/java/com/jakubadamek/robotemil/htmlparser/LastminuteEs.java
@@ -1,7 +1,6 @@
 package com.jakubadamek.robotemil.htmlparser;
 
 import java.io.IOException;
-import java.util.Calendar;
 
 import org.apache.log4j.Logger;
 import org.htmlparser.Node;
@@ -13,6 +12,9 @@ import org.htmlparser.filters.HasParentFilter;
 import org.htmlparser.filters.OrFilter;
 import org.htmlparser.filters.TagNameFilter;
 import org.htmlparser.util.ParserException;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 /**
  * Parses lastminute.es
@@ -22,34 +24,19 @@ import org.htmlparser.util.ParserException;
 public class LastminuteEs extends HtmlParser {
     private final Logger logger = Logger.getLogger(getClass());
 	private static final String LASTMINUTE_COM =
-		"http://www.es.lastminute.com/site/viajes/hoteles/hotels-results.html" +
-		"?skin=eses.lastminute.com&lmnRooms=1" +
-		"&returnURL=http%3A%2F%2Fwww.es.lastminute.com%2Fsite%2Fmain%2Fhomepage_es_ES.html" +
-		"&preserveName-CATEGORY=hotels" +
-		"&preserveName-requestURL=%2Fsite%2Fmain%2Fhomepage_es_ES.html" +
-		"&lmnShowRestaurants=False&lmnLengthOfStay=1" +
-		"&lmnRoom2ChildAge2=-1&lmnRoom2ChildAge1=-1&lmnRoom3ChildAge1=-1" +
-		"&lmnChildrenRoom3=0&lmnChildrenRoom2=0&lmnChildrenRoom1=0&preserveName-CATID=4" +
-		"&lmnRoom3ChildAge3=-1&lmnRoom2ChildAge3=-1&lmnHotelName=&method=hotelsFullSearch" +
-		"&lmnRoom2ChildAge4=-1&CATID=4&preserveName-SEARCH=basic&lmnRoom3ChildAge2=-1" +
-		"&errorURL=http%3A%2F%2Fwww.es.lastminute.com%2Fsite%2Fviajes%2Fhoteles%2Ferror.html" +
-		"&filterResultsBy=all&searchTypeName=cityName" +
-		"&lmnCountry=CZ|CZECH+REPUBLIC|CZECH+REPUBLIC&lmnAdultsRoom3=2&lmnAdultsRoom2=2&lmnAdultsRoom1=1" +
-		"&lmnRoom1ChildAge3=-1&lmnRoom1ChildAge2=-1&lmnRoom1ChildAge1=-1&lmnLocation=Prague" +
-		"&lmnRoom3ChildAge4=-1&lmnRoom1ChildAge4=-1&preserveName-skin=eses.lastminute.com";
+		"http://www.es.lastminute.com/trips/hotellist/listInternal" +
+		"?hotelMaxReturnPerPage=25" +
+		"&guestCounts=1&guestCodes=ADULT&city=PRG" +
+		"&numRooms=1&path=hotels";
 
 	@Override
 	public boolean run() throws ParserException, IOException {
 		String url = LASTMINUTE_COM;
-	    Calendar calendar = Calendar.getInstance();
-	    calendar.setTime(this.dateFrom);
-	    //&lmnCheckOutMonth=8&lmnCheckInDay=15&lmnCheckOutDay=16&lmnCheckInMonth=8
+	    //&checkInDate=2013-01-12&checkOutDate=2013-01-13
 	    //&startIndex=26
-	    url += "&lmnCheckInDay=" + calendar.get(Calendar.DAY_OF_MONTH);
-	    url += "&lmnCheckInMonth=" + (calendar.get(Calendar.MONTH) + 1);
-	    calendar.setTime(this.dateTo.toDate());
-	    url += "&lmnCheckOutDay=" + calendar.get(Calendar.DAY_OF_MONTH);
-	    url += "&lmnCheckOutMonth=" + (calendar.get(Calendar.MONTH) + 1);
+	    DateTimeFormatter dateFormat = DateTimeFormat.forPattern("yyyy-MM-dd");
+	    url += "&checkInDate=" + dateFormat.print(new DateTime(this.dateFrom));
+	    url += "&checkOutDate=" + dateFormat.print(this.dateTo);
 		logger.debug(url);
 		NodeFilter hotelNameFilter = 
 			new AndFilter(
@@ -57,8 +44,12 @@ public class LastminuteEs extends HtmlParser {
 					new HasParentFilter(new TagNameFilter("h3")));
 		NodeFilter priceFilter = 
 			new AndFilter(
-					new TagNameFilter("span"),
-					new HasAttributeFilter("class", "text_inverse"));
+					new TagNameFilter("strong"),
+					new HasAttributeFilter("class", "number highlight has-tooltip cursor-pointer"));
+		NodeFilter priceFilter2 = 
+			new AndFilter(
+					new TagNameFilter("div"),
+					new HasAttributeFilter("class", "product-price product-price-h101"));				
 		NodeFilter takeAllFilter = new OrFilter(hotelNameFilter, priceFilter); 
 		int ipage = 0;
 		int pageHotels = 1;
@@ -77,14 +68,13 @@ public class LastminuteEs extends HtmlParser {
 			    if(isStop()) return false;
 				if(hotelNameFilter.accept(node)) {
 					hotel = node.getChildren().toNodeArray()[0].getText().replace("- Praga", "").trim();
+					logger.debug("Found hotel: " + hotel);
 				}
 				if(priceFilter.accept(node)) {
-					if(node.getChildren().size() > 2) {
-						price = node.getChildren().toNodeArray()[2].getText().trim();
-					} else {
-						price = node.getChildren().toNodeArray()[0].getText().trim();
-					}
-					price = price.replace("&euro;", "").replace("&nbsp;", "").trim();
+					logger.debug("Found price: " + node.getNextSibling());
+					price = node.getNextSibling().getText().trim();
+					price = price.replace("&#8364;", "").replace("&nbsp;", "").trim();
+					price = price.replace(",", ".");
 					if(price.length() > 0 && hotel != "") {
 						addPrice(hotel, this.key, price, false);
 						pageHotels ++;
